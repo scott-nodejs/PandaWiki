6f4879383d5f52138e7ae8ce2fa2cbafdc8356f8
lucong <lucong@teddymobile.cn>
2025-08-03 11:18:20 +0800

    init

--- backend_java/src/main/java/com/chaitin/pandawiki/service/ai/assistant/IAssistant/WebSearchAssistant.java
+++
@@ -1,9 +0,0 @@
-package com.chaitin.pandawiki.service.ai.assistant.IAssistant;
-
-/**
- * @author: iohw
- * @date: 2025/4/23 21:32
- * @description:
- */
-public interface WebSearchAssistant extends StreamingAssistant{
-}
